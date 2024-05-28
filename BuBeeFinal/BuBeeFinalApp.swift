6e4afe83c2cfd0be459e2d066d6cf7e44fc21862
reinexxism <lucy0805@naver.com>
2024-05-28 15:43:53 +0900

    Initial Commit

---
+++ BuBeeFinal/BuBeeFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuBeeFinalApp.swift
+//  BuBeeFinal
+//
+//  Created by 최주현 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BuBeeFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
